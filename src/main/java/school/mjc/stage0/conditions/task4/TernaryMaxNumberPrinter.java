d02e526e829f3c9b530d55d997eac8b0a309eb73
Azim Shamuratov <a.shamuratov@atmos.uz>
2022-09-13 14:40:16 +0500

    Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first >= second && first >= third ? first : second > first && second > third ? second : third > first && third> second ? third : second;
+
+        System.out.println(result);
     }
 }
